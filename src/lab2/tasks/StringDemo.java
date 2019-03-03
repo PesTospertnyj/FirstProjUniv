455169242d38eb37e383e1d86e6ded1555671554
PesTospertnyj <gonartur@ukr.net>
2019-03-03 20:24:39 +0200

    Done

--- src/lab2/tasks/StringDemo.java
+++ src/lab2/tasks/StringDemo.java
@@ -4,35 +4,37 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class StringDemo {
-	public static void main(String[] args) {
-		int strNumber = 0;
-		StringDemo sd = new StringDemo();
-		while (true) {
-			System.out.println("Сколько строк желаете ввести?");
-			try {
-				Scanner sn = new Scanner(System.in);
-				strNumber = sn.nextInt();
-				break;
-			} catch (InputMismatchException e) {
-				System.out.println("Ошибка! Ведите целое число строк!");
-			}
-		}
-		String strArr[] = new String[strNumber];
-		Scanner scan = new Scanner(System.in);
-		for (int i = 0; i < strNumber; i++) {
-			System.out.println("Введите строку " + (i + 1));
-			strArr[i] = scan.nextLine();
-		}
-		int strSum = 0;
-		int strAvg = 0;
-		for (int i = 0; i< strArr.length; i++) {
-			strSum += strArr[i].length();
-			
-		}
-		strAvg = strSum / strNumber;
-		System.out.println("Строки с длинной меньше средней и их длинны! ");
-//		for (int i = 0; i < ) {
-//			System.out.println(strArr[i]);
-//		}
-	}
+    public static void main(String[] args) {
+        int strNumber;
+        while (true) {
+            System.out.println("Сколько строк желаете ввести?");
+            try {
+                Scanner sn = new Scanner(System.in);
+                strNumber = sn.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Ошибка! Ведите целое число строк!");
+            }
+        }
+        String strArr[] = new String[strNumber];
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < strNumber; i++) {
+            System.out.println("Введите строку " + (i + 1));
+            strArr[i] = scan.nextLine();
+        }
+        int strSum = 0;
+        int strAvg = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            strSum += strArr[i].length();
+        }
+        strAvg = strSum / strNumber;
+        System.out.println("Строки с длинной меньше средней и их длинны! ");
+
+        for (String line: strArr) {
+            if (line.length() < strAvg) {
+                System.out.println(line + ", " + line.length());
+            }
+        }
+
+    }
 }
