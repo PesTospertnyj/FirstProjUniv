1c3211e1ac29f1fb0bcd4797bd9ab8f1ac5148a8
artur <artur@artur-HP-Compaq-Elite-8300-SFF>
2019-03-05 23:41:53 +0200

    1

--- src/lab3/examples/Groups.java
+++ src/lab3/examples/Groups.java
@@ -1,5 +1,24 @@
 package lab3.examples;
 
-public class Groups {
+import java.util.regex.*;
 
+public class Groups {
+	public static void main(String[] args) {
+		String input = "abdcxyz";
+		myMatches("([a-z]*)([a-z]+)", input);
+		myMatches("([a-z]?)([a-z]+)", input);
+		myMatches("([a-z]+)([a-z]*)", input);
+		myMatches("([a-z]?)([a-z]?)", input);
+	}
+
+	public static void myMatches(String regex, String input) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(input);
+		if (matcher.matches()) {
+			System.out.println("First group: " + matcher.group(1));
+			System.out.println("Second group: " + matcher.group(2));
+		} else
+			System.out.println("nothing");
+		System.out.println();
+	}
 }
