3494098c4d62c1b47c7429173840677dce5cc8b4
artur <artur@artur-HP-Compaq-Elite-8300-SFF>
2019-03-05 23:31:14 +0200

    second task done

--- src/lab2/tasks/StringDemo.java
+++ src/lab2/tasks/StringDemo.java
@@ -7,19 +7,19 @@ public class StringDemo {
     public static void main(String[] args) {
         int strNumber;
         while (true) {
-            System.out.println("Сколько строк желаете ввести?");
+            System.out.println("How many Strings do you want to insert?");
             try {
                 Scanner sn = new Scanner(System.in);
                 strNumber = sn.nextInt();
                 break;
             } catch (InputMismatchException e) {
-                System.out.println("Ошибка! Ведите целое число строк!");
+                System.out.println("Warning! You inserted not a String!");
             }
         }
         String strArr[] = new String[strNumber];
         Scanner scan = new Scanner(System.in);
         for (int i = 0; i < strNumber; i++) {
-            System.out.println("Введите строку " + (i + 1));
+            System.out.println("String number " + (i + 1));
             strArr[i] = scan.nextLine();
         }
         int strSum = 0;
@@ -28,7 +28,7 @@ public class StringDemo {
             strSum += strArr[i].length();
         }
         strAvg = strSum / strNumber;
-        System.out.println("Строки с длинной меньше средней и их длинны! ");
+        System.out.println("Strings with length less, then avg length and number of symbols! ");
 
         for (String line: strArr) {
             if (line.length() < strAvg) {
